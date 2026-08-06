30d65eb9c58d467f8bfa2bbd5bd7a784f4f433a5
RAj Gulhane <rajgulhane3@gmail.com>
2026-08-06 16:53:03 +0530

    changes status code 1

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -17,7 +17,7 @@ api.interceptors.request.use((config) => {
 api.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response?.status === 40) {
+    if (error.response?.status === 401) {
       localStorage.removeItem('bustrack_token')
       localStorage.removeItem('bustrack_user')
       window.location.href = '/login'
